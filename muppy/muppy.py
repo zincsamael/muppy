5a6fab1a5255a3c721cf8e53126a69d10503c2f1
okkoto <okkoto@065d0e52-c953-0410-8687-a5fe83da3a3f>
2008-06-05 09:25:09 +0000

    added print_summary function

--- muppy/muppy.py
+++ muppy/muppy.py
@@ -1,9 +1,18 @@
 import gc
+import string
 import sys
+
 
 def get_objects():
     """Return a list of all known objects. """
     return gc.get_objects()
+
+def get_size(objects):
+    """Compute the size of all objects."""
+    res = 0
+    for o in objects:
+        res += sys.getsizeof(o)
+    return res
 
 def sort(objects):
     """Sort objects by size in bytes."""
@@ -26,14 +35,89 @@ def get_referents(object, level=1):
     """Get all referents of an object up to a certain level.
     
     This function is recursive."""
-
     res = gc.get_referents(object)
     level -= 1
     if level > 0:
         for o in res:
             res.extend(get_referents(o, level))
+    res = _remove_duplicates(res)
     return res
 
 def _remove_duplicates(objects):
-    return list(set(objects))
-    
+    res = []
+    [res.append(o) for o in objects if not o in res]
+    return res
+
+def print_summary(objects, limit=15, sort='size', order='descending'):
+    """Print a memory usage summary for a passed objects.
+
+    Keyword arguments:
+    limit -- the maximum number of elements to be listed
+    sort  -- sort elements by 'size', 'type', or '#'
+    order -- sort 'ascending' or 'descending'
+
+    """
+    # input validation
+    sorts = ['type', '#', 'size']
+    if sort not in sorts:
+        raise ValueError("invalid sort, should be one of" + str(sorts))
+    orders = ['ascending', 'descending']
+    if order not in orders:
+        raise ValueError("invalid order, should be one of" + str(orders))
+    # summarize
+    summary = {}
+    for o in objects:
+        otype = type(o)
+        if otype in summary:
+            summary[otype].append(sys.getsizeof(o))
+        else:
+            summary[otype] = []
+            summary[otype].append(sys.getsizeof(o))
+    # build rows
+    rows = []
+    for otype, sizes in summary.iteritems():
+        rows.append([otype, len(sizes), sum(sizes)])
+    # sort rows
+    if sorts.index(sort) == 0:
+        if order == "ascending":
+            rows.sort(lambda r1, r2: cmp(str(r1[0]),str(r2[0])))
+        elif order == "descending":
+            rows.sort(lambda r1, r2: -cmp(str(r1[0]),str(r2[0])))
+    else: 
+        if order == "ascending":
+            rows.sort(lambda r1, r2: r1[sorts.index(sort)] - r2[sorts.index(sort)])
+        elif order == "descending":
+            rows.sort(lambda r1, r2: r2[sorts.index(sort)] - r1[sorts.index(sort)])
+    # limit rows
+    rows = rows[0:limit]
+    # print rows
+    rows.insert(0,["types", "# objects", "total size"])
+    _print_table(rows)
+
+def _print_table(rows, header=True):
+        """ Outputs a list of lists as a pr
+
+        - rows - list of lists
+        - header - if True the first row is treated as a table header
+
+        inspired by http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/267662
+        """
+        border="="
+        # vertical delimiter
+        vdelim = " | "
+        # padding nr. of spaces are left around the longest element in the
+        # column
+        padding=1
+        # may be left,center,right
+        justify='right'
+        justify = {'left':string.ljust,'center':string.center,\
+                       'right':string.rjust}[justify.lower()]
+        # calculate column widths (longest item in each col
+        # plus "padding" nr of spaces on both sides)
+        cols = zip(*rows)
+        colWidths = [max([len(str(item))+2*padding for item in col]) for col in cols]
+        borderline = vdelim.join([w*border for w in colWidths])
+        for row in rows: 
+            print vdelim.join([justify(str(item),width) for (item,width) in zip(row,colWidths)])
+            if header: print borderline; header=False
+
--- muppy/test/test_muppy.py
+++ muppy/test/test_muppy.py
@@ -1,6 +1,7 @@
-import unittest
-import sys
+import doctest
 import random 
+import sys
+import unittest
 
 import muppy
 
@@ -68,11 +69,19 @@ class MuppyTest(unittest.TestCase):
         for o in res:
             self.assert_((o in l1) or (o in l2))
         
+    def test_getsize(self):
+        """Check that the return value is the sum of the size of all objects."""
+        (o1, o2, o3, o4, o5) = (1, 'a', 'b', 4, 5)
+        list = [o1, o2, o3, o4, o5]
+        expected = 0
+        for o in list:
+            expected += sys.getsizeof(o)
 
-        
+        self.assertEqual(muppy.get_size(list), expected)
+
     def test_remove_duplicates(self):
         """Verify that this operations returns a duplicate-free lists. 
-
+        
         That, is no objects are listed twice. This does not apply to objects
         with same values."""
         (o1, o2, o3, o4, o5) = (1, 'a', 'b', 4, 5)
@@ -84,8 +93,80 @@ class MuppyTest(unittest.TestCase):
             self.assert_(o in expected)
 
 
+test_print_table = """
+
+The _print_table function should print a nice, clean table.
+
+>>> r1 = ["types", "#objects", "total size"]
+>>> r2 = [str, 17, 442]
+>>> r3 = [dict, 4, 9126]
+>>> r4 = [list, 11, 781235]
+>>> muppy._print_table([r1, r2, r3, r4])
+          types |   #objects |   total size
+=============== | ========== | ============
+   <type 'str'> |         17 |          442
+  <type 'dict'> |          4 |         9126
+  <type 'list'> |         11 |       781235
+"""
+  
+test_print_summary = """
+>>> objects = [1,2,3,4,5L, 33000L, "a", "ab", "abc", [], {}, {10: "t"}, ]
+
+At first the default values.
+>>> muppy.print_summary(objects)
+          types |   # objects |   total size
+=============== | =========== | ============
+  <type 'dict'> |           2 |          512
+   <type 'str'> |           3 |          126
+   <type 'int'> |           4 |           96
+  <type 'long'> |           2 |           66
+  <type 'list'> |           1 |           40
+
+Next, we try it sorted by object number
+>>> muppy.print_summary(objects, sort='#')
+          types |   # objects |   total size
+=============== | =========== | ============
+   <type 'int'> |           4 |           96
+   <type 'str'> |           3 |          126
+  <type 'long'> |           2 |           66
+  <type 'dict'> |           2 |          512
+  <type 'list'> |           1 |           40
+
+Now, object number and with ascending order
+>>> muppy.print_summary(objects, sort='#', order='ascending')
+          types |   # objects |   total size
+=============== | =========== | ============
+  <type 'list'> |           1 |           40
+  <type 'long'> |           2 |           66
+  <type 'dict'> |           2 |          512
+   <type 'str'> |           3 |          126
+   <type 'int'> |           4 |           96
+
+Let's limit the output to two rows
+>>> muppy.print_summary(objects, limit=2, sort='#', order='ascending')
+          types |   # objects |   total size
+=============== | =========== | ============
+  <type 'list'> |           1 |           40
+  <type 'long'> |           2 |           66
+
+Finally, sorted by size with descending order
+>>> muppy.print_summary(objects, sort='size', order='descending')
+          types |   # objects |   total size
+=============== | =========== | ============
+  <type 'dict'> |           2 |          512
+   <type 'str'> |           3 |          126
+   <type 'int'> |           4 |           96
+  <type 'long'> |           2 |           66
+  <type 'list'> |           1 |           40
+"""
+      
+__test__ = {"test_print_table": test_print_table,\
+            "test_print_summary": test_print_summary}
+
 def suite():
-    return unittest.makeSuite(MuppyTest,'test')
+    suite = unittest.makeSuite(MuppyTest,'test') 
+    suite.addTest(doctest.DocTestSuite())
+    return suite
 
 if __name__ == '__main__':
     unittest.main()
