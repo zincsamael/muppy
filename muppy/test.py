13c25d4060951afd707d02cc71ff3cf0e512cda8
okkoto <okkoto@065d0e52-c953-0410-8687-a5fe83da3a3f>
2008-06-17 14:42:55 +0000

    added simple profiler class

--- muppy/test.py
+++ muppy/test.py
@@ -2,7 +2,9 @@ import unittest
 
 import muppy
 from test import test_muppy
+from test import test_mprofile
 
 suite = test_muppy.suite()
+suite.addTest(test_mprofile.suite())
 unittest.TextTestRunner(verbosity=2).run(suite)
 
