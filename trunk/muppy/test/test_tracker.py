4af58f7010f7e1f5827b4cef90292c39f73d6b78
robert.schuppenies <robert.schuppenies@065d0e52-c953-0410-8687-a5fe83da3a3f>
2008-08-07 18:17:36 +0000

    Renamed tracker to SummaryTracker, added ObjectTracker.

--- trunk/muppy/test/test_tracker.py
+++ trunk/muppy/test/test_tracker.py
@@ -11,26 +11,21 @@ import bz2
 class TrackerTest(unittest.TestCase):
 
     def setUp(self):
-        self.tracker = tracker.tracker()
-        gc.collect()
-
-    def tearDown(self):
-        self.tracker = None
         gc.collect()
 
     def _get_indicator(self):
         """Create an indicattor object to track changes between snashots."""
         return bz2.BZ2Compressor()
         
-    def _contains_indicator(self, snapshot):
-        """How many indicator objects does the snapshot contain."""
+    def _contains_indicator(self, summary):
+        """How many indicator objects does the summary contain."""
         res = None
-        for row in snapshot:
+        for row in summary:
             if row[0].find('bz2.BZ2Compressor')!= -1:
                 res = row[1]
         return res
         
-    def test_diff(self):
+    def test_stracker_diff(self):
         """Check that the diff is computed correctly.
 
         This includes that
@@ -38,98 +33,131 @@ class TrackerTest(unittest.TestCase):
         - removed objects are not listed anymore
         - if objects disappear, they should be listed as negatives
         """
+        stracker = tracker.SummaryTracker()
         # for now, no object should be listed
         gc.collect()
-        diff = self.tracker.diff()
+        diff = stracker.diff()
         self.assert_(self._contains_indicator(diff) == None)
         # now an indicator object should be included in the diff
         o = self._get_indicator()
         gc.collect()
-        diff = self.tracker.diff()
+        diff = stracker.diff()
         self.assert_(self._contains_indicator(diff) == 1)
         # now it should be gone again, compared to the
-        # previously stored snapshot
+        # previously stored summary
         o = self._get_indicator()
         gc.collect()
-        sn1 = self.tracker._make_snapshot()
+        sn1 = stracker.create_summary()
         o = None
         gc.collect()
-        diff = self.tracker.diff(snapshot1=sn1)
+        diff = stracker.diff(summary1=sn1)
         self.assert_(self._contains_indicator(diff) == -1)
-        # comparing two homemade snapshots should work, too
+        # comparing two homemade summaries should work, too
         gc.collect()
         o = None
-        sn1 = self.tracker._make_snapshot()
+        sn1 = stracker.create_summary()
         o = self._get_indicator()
         gc.collect()
-        sn2 = self.tracker._make_snapshot()
-        diff = self.tracker.diff(snapshot1=sn1, snapshot2=sn2)
+        sn2 = stracker.create_summary()
+        diff = stracker.diff(summary1=sn1, summary2=sn2)
         self.assert_(self._contains_indicator(diff) == 1)
-        # providing snapshot2 without snapshot1 should raise an exception
-        self.assertRaises(ValueError, self.tracker.diff, snapshot2=sn2)
+        # providing summary2 without summary1 should raise an exception
+        self.assertRaises(ValueError, stracker.diff, summary2=sn2)
 
-    def test_for_leaks_in_tracker(self):
+    def test_stracker_for_leaks_in_tracker(self):
         """Test if any operations of the tracker leak memory."""
 
-        # test _make_snapshot
-        tmp_tracker = tracker.tracker()
+        # test create_summary
+        tmp_tracker = tracker.SummaryTracker()
         # XXX: TODO
-#        self.assert_(muppy.get_usage(tmp_tracker._make_snapshot) == None)
-#        self.assert_(muppy.get_usage(tmp_tracker.store_snapshot, 1) == None)
+#        self.assert_(muppy.get_usage(tmp_tracker.create_summary) == None)
+#        self.assert_(muppy.get_usage(tmp_tracker.store_summary, 1) == None)
         # test print_diff
 #        self.assert_(muppy.get_usage(tmp_tracker.print_diff, [], []) == None)
         
-    def test_make_snapshot(self):
-        """Check that a snapshot is created correctly.
+    def test_stracker_create_summary(self):
+        """Check that a summary is created correctly.
         
         This can only be done heuristically, e.g that most recent objects are
         included.
-        Also check that snapshots managed by the tracker are excluded if
+        Also check that summaries managed by the tracker are excluded if
         ignore_self is enabled.
 
         """
         # at the beginning, there should not be an indicator object listed
-        tmp_tracker = tracker.tracker()
+        tmp_tracker = tracker.SummaryTracker()
         gc.collect()
-        sn = tmp_tracker._make_snapshot()
+        sn = tmp_tracker.create_summary()
         self.assert_(self._contains_indicator(sn) == None)
         # now an indicator object should be listed
         o = self._get_indicator()
         gc.collect()
-        sn = tmp_tracker._make_snapshot()
+        sn = tmp_tracker.create_summary()
         self.assert_(self._contains_indicator(sn) == 1)
-
-        # with ignore_self enabled a second snapshot should not list the first
-        # snapshot
-        sn = tmp_tracker._make_snapshot()
-        sn2 = tmp_tracker._make_snapshot()
+        # with ignore_self enabled a second summary should not list the first
+        # summary
+        gc.collect()
+        sn = tmp_tracker.create_summary()
+        gc.collect()
+        sn2 = tmp_tracker.create_summary()
         tmp = summary._sweep(summary.get_diff(sn, sn2))
-        print tmp
         self.assert_(len(tmp) == 0)
         # but with ignore_self turned off, there should be some difference
-        tmp_tracker = tracker.tracker(ignore_self=False)
-        sn = tmp_tracker._make_snapshot()
-        sn2 = tmp_tracker._make_snapshot()
+        tmp_tracker = tracker.SummaryTracker(ignore_self=False)
+        sn = tmp_tracker.create_summary()
+        sn2 = tmp_tracker.create_summary()
         tmp = summary._sweep(summary.get_diff(sn, sn2))
         self.assert_(len(tmp) != 0)
         
-    def test_store_snapshot(self):
-        """Check that a snapshot is stored under the correct key and most
+    def test_stracker_store_summary(self):
+        """Check that a summary is stored under the correct key and most
         recent objects are included.
 
         """
+        stracker = tracker.SummaryTracker()
         key = 1
-        self.tracker.store_snapshot(key)
-        s = self.tracker.snapshots[key]
+        stracker.store_summary(key)
+        s = stracker.summaries[key]
         self.assert_(s != None)
         # check that indicator 
         key = 2
         tmp = self._get_indicator()
-        self.tracker.store_snapshot(key)
-        s = self.tracker.snapshots[key]
+        stracker.store_summary(key)
+        s = stracker.summaries[key]
         self.assert_(self._contains_indicator(s) == 1)
 
-       
+#
+# now the tests for the object tracker
+#
+    def test_otracker_get_objects(self):
+        otracker = tracker.ObjectTracker()
+        o = self._get_indicator()
+        # indicator object should be in result set
+        self.assert_(o in otracker._get_objects())
+        # indicator object should not be in result set
+        self.assert_(o not in otracker._get_objects(ignore=[o]))
+
+    def test_otracker_diff(self):
+        import inspect
+        # indicator object should be listed in diff
+        otracker = tracker.ObjectTracker()
+        o = self._get_indicator()
+        diff = otracker.get_diff()
+        self.assert_(o in diff['+'])
+        # indicator should not be listed in diff, i.e. no new and no gone
+        # indicator object
+        diff = otracker.get_diff(ignore=[inspect.currentframe()])
+        found = False
+        tmp = self._get_indicator()
+        for i in diff['+']:
+            if isinstance(i, type(tmp)):
+                found = True
+        self.assert_(not found)
+        for i in diff['-']:
+            if isinstance(i, type(tmp)):
+                found = True
+        self.assert_(not found)
+
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(TrackerTest)
 
