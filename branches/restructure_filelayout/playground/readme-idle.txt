e082ee6de94d39fe45d117308276f924f19e088e
robert.schuppenies <robert.schuppenies@065d0e52-c953-0410-8687-a5fe83da3a3f>
2008-08-13 07:45:47 +0000

    Removed obsolete branch.

--- branches/restructure_filelayout/playground/readme-idle.txt
+++
@@ -1,4 +0,0 @@
-You will need to run 'python start.py' in order to execute the IDLE from this
-directory, not the one from the python installation.
-
-
--- branches/restructure_filelayout/playground/start-idle.py
+++
@@ -1,8 +0,0 @@
-# this is required in order that the IDLE from this directory is executed,
-# not the imported from the python installation
-import os, sys
-sys.path.insert(0, os.curdir)
-
-import idlelib.idle
-
-
